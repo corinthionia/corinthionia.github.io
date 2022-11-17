29366d19c24b699cde98bf71475a3d6de244b7a6
Joohyun Kim <corinthionia@gmail.com>
2022-11-17 14:41:11 +0900

    :iphone: design: work on responsive design

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -3,6 +3,8 @@ import { graphql } from 'gatsby';
 import { PostPageItemType } from 'types/PostItem.types'; // 바로 아래에서 정의할 것입니다
 import Template from 'components/common/Template';
 import PostHead from 'components/post/PostHead';
+import PostContent from 'components/post/PostContent';
+import CommentWidget from 'components/post/CommentWidget';
 
 type PostTemplateProps = {
   data: {
@@ -18,28 +20,14 @@ const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
   },
 }) {
   const {
-    node: {
-      html,
-      frontmatter: {
-        title,
-        summary, // 나중에 사용할 예정입니다!
-        date,
-        categories,
-        thumbnail: {
-          childImageSharp: { gatsbyImageData },
-        },
-      },
-    },
+    node: { html, frontmatter },
   } = edges[0];
 
   return (
     <Template>
-      <PostHead
-        title={title}
-        date={date}
-        categories={categories}
-        thumbnail={gatsbyImageData}
-      />
+      <PostHead {...frontmatter} />
+      <PostContent html={html} />
+      <CommentWidget />
     </Template>
   );
 };
