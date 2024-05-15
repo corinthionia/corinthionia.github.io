705c6ddb145d90cffdfc5c2b46d80c5ac16e3b30
Joohyun Kim <corinthionia@gmail.com>
2024-05-16 03:38:05 +0900

    feat: apply local font

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,10 +1,4 @@
-import { Noto_Sans_KR } from 'next/font/google';
 import localFont from 'next/font/local';
-
-export const NotoSansKR = Noto_Sans_KR({
-  weight: ['200', '300', '400', '500', '600', '700'],
-  subsets: ['latin'],
-});
 
 export const Pretendard = localFont({
   src: '../assets/fonts/Pretendard.woff2',
