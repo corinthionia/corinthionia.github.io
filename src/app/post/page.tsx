6943dd4f7ab0ffe2992229d0723898cd123a2b92
Joohyun Kim <corinthionia@gmail.com>
2024-05-16 22:55:22 +0900

    feat: add post list page

---
+++ src/app/post/page.tsx
@@ -0,0 +1,16 @@
+import Thumbnail from '@/components/thumbnail/Thumbnail';
+import { PostType } from '@/interfaces/post';
+import { getAllPosts } from 'src/utils/post';
+import styles from './index.module.scss';
+
+export default async function Post() {
+  const posts = await getAllPosts();
+
+  return (
+    <div className={styles.thumbnailList}>
+      {posts.map((post: PostType) => (
+        <Thumbnail key={post.fields.slug} {...post} />
+      ))}
+    </div>
+  );
+}
