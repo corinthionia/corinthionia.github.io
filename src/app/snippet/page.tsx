54e99f8dc46c59bcf212974d6f319a427db8b520
Joohyun Kim <corinthionia@gmail.com>
2024-05-20 16:06:51 +0900

    feat: add responsive design to snippet page

--- src/app/snippet/page.tsx
+++ src/app/snippet/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
   const posts = await getAllSnippets();
 
   return (
-    <main className={styles.thumbnailList}>
+    <main className={styles.wrapper}>
       {posts.map((post: PostType) => (
         <Snippet key={post.fields.slug} title={post.frontMatter.title} content={post.content} />
       ))}
