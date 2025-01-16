1ca86916ea3f039aeaae4a6ae74472be59ab1548
Joohyun Kim <corinthionia@gmail.com>
2025-01-17 00:56:28 +0900

    Updates

---
+++ _next/static/PkMo4GYYPLAsTuK4jXIFy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-8076d9a07c1800c0.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/zRkNL4-RoCE95hpsRW9Cr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-d9d1c407513f324e.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
